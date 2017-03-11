ca594b63cabac50de3cafc3dd53c7bc26103d0fc
shiranmazor <shiran.mazpr@gmail.com>
2017-03-11 21:03:05 +0200

    c3

--- help_module.py
+++ help_module.py
@@ -48,3 +48,76 @@ def create_wageGap_graph_tuple(data_no_na,countries_count,remaining_features,x):
     # ploting average in blue color
     plot_tuple = plot_tuple + (years, y_graphs_dict['WageGapAvg'], 'b')
     return plot_tuple, country_colums_list,feature_countries
+
+def create_graph_test_prediction_vs_data(data_no_na,traintest_threshold,y_hat_test_sklearn):
+
+    test = data_no_na[data_no_na['Year'] >= traintest_threshold]
+
+    plot_data = test[['Year', 'WageGaP']]
+    plot_data = plot_data.groupby('Year').mean()
+
+    cols = test.columns.tolist()
+    test_2 = test.reset_index(drop=True)
+    test_2.columns = cols
+
+    df_y_hat_sklearn = pd.DataFrame(data=y_hat_test_sklearn, columns=['WageGaPPredict'])
+
+    df_prediction_sklearn_only_test = pd.concat([test_2, df_y_hat_sklearn], axis=1, join='inner')
+    plot_Prediction_sklearn_only_test = df_prediction_sklearn_only_test[['Year', 'WageGaPPredict']]
+    plot_Prediction_sklearn_only_test = plot_Prediction_sklearn_only_test.groupby('Year').mean()
+
+    lines = plt.plot(plot_data.index, plot_data.WageGaP, color='r')
+    lines = plt.plot(plot_Prediction_sklearn_only_test.index, plot_Prediction_sklearn_only_test.WageGaPPredict,
+                     color='b')
+
+    plt.ylabel('Average Wage Gap ratio')
+    plt.xlabel('Years')
+    plt.title('Average Wage Gap prediction (blue-sklearn) vs data(red)-only test')
+    plt.ylim(0, 30)
+    plt.xlim(traintest_threshold, 2015)
+    plt.ticklabel_format(useOffset=False)
+    plt.show()
+
+def create_error_graph_test_pred_vs_data(data_no_na,traintest_threshold,y_hat_test_sklearn):
+
+    test = data_no_na[data_no_na['Year'] >= traintest_threshold]
+
+    cols = test.columns.tolist()
+    test_2 = test.reset_index(drop=True)
+    test_2.columns = cols
+
+    df_y_hat_sklearn_only_test = pd.DataFrame(data=y_hat_test_sklearn, columns=['WageGaPPredict'])
+
+    df_prediction_and_data_sklearn = pd.concat([test_2, df_y_hat_sklearn_only_test], axis=1, join='inner')
+
+    df_data_sklearn = df_prediction_and_data_sklearn[['Year', 'WageGaPPredict']]
+    df_prediction_sklearn = df_prediction_and_data_sklearn[['Year', 'WageGaP']]
+
+    df_data_sklearn_mean = df_data_sklearn.groupby('Year').mean()
+    df_prediction_sklearn_mean = df_prediction_sklearn.groupby('Year').mean()
+
+    df_prediction_and_data_sklearn_mean = pd.concat([df_data_sklearn_mean, df_prediction_sklearn_mean], axis=1,
+                                                    join='inner')
+
+    precentage_error_sklearn = ((abs(
+        df_prediction_and_data_sklearn_mean['WageGaPPredict'] - df_prediction_and_data_sklearn_mean['WageGaP']) /
+                                 df_prediction_and_data_sklearn_mean['WageGaP']) * 100)
+    df_precentage_error_sklearn = pd.DataFrame(data=precentage_error_sklearn, columns=['PrecentageErr'])
+
+    plot_precentageErr_sklearn = pd.concat([df_prediction_and_data_sklearn_mean, df_precentage_error_sklearn], axis=1,
+                                           join='inner')
+
+    lines = plt.plot(plot_precentageErr_sklearn.index, plot_precentageErr_sklearn.PrecentageErr, color='g')
+
+    plt.ylabel('Error in Precentage over the years')
+    plt.xlabel('Years')
+    plt.title('Average Precentage Error ')
+    plt.ylim(0, 40)
+    plt.ticklabel_format(useOffset=False)
+    plt.show()
+
+    mean_err = np.mean(plot_precentageErr_sklearn.PrecentageErr)
+    max_err = np.max(plot_precentageErr_sklearn.PrecentageErr)
+
+    print('average error in precentage: %.2f' % mean_err)
+    print('max error in precentage: %.2f' % max_err)
